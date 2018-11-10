b39e82734efa0d08a5a9c2bfc1676f337a8e6e98
degwinthegreat <the.colledge.dropout@gmail.com>
2018-11-10 17:40:29 +0900

    ログイン機能実装

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # README
 
+# 現場で使えるRUBY ON RAILS 5 速習ガイド 勉強中
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,18 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_includeing_comma
+
+  belongs_to :user
+
+  private
+
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
+
+  def validate_name_not_includeing_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
